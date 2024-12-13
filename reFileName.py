1cb03cd64e82088fc271f9c3deb318c2c3bd8c21
B635 <frankxck@gmail.com>
2024-12-13 08:23:43 +0800

    Add reFileName.py file

---
+++ reFileName.py
@@ -0,0 +1,14 @@
+import os
+
+def rename_files_in_directory(directory):
+    for filename in os.listdir(directory):
+        if filename.startswith("VCG"):
+            new_filename = filename[len("VCG"):]
+            old_file_path = os.path.join(directory, filename)
+            new_file_path = os.path.join(directory, new_filename)
+            os.rename(old_file_path, new_file_path)
+            print(f'Renamed: "{filename}" to "{new_filename}"')
+
+# 使用示例
+directory_path = 'you own path'
+rename_files_in_directory(directory_path)
